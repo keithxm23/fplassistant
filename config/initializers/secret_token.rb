2f315b51b7df49db85f019728cbd3e6dc2c33ae4
Keith Mascarenhas <kmascare@akamai.com>
2013-08-16 10:54:03 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fpl::Application.config.secret_token = '46224f90e48b04d2ee299c5cc52c8f4c4f41992b9437ef969125ac6f55d3ee624170b7dee86cebc3be3d83f27309b7023d7d91ab1a8cffb0dc3fbb59a8afd0e1'
