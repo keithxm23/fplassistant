f96850eda047f41d0f74500db600ea68ec0f9055
Keith Mascarenhas <keithxm23@gmail.com>
2013-10-04 01:53:09 +0000

    only display players who have status != u, that is those that still play in premier league

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,7 +2,7 @@ class PlayersController < ApplicationController
   # GET /players
   # GET /players.json
   def index
-    @players = Player.all
+    @players = Player.where("status != 'u'")
     
     jsondata = {}
     jsondata['total'] = "50"
